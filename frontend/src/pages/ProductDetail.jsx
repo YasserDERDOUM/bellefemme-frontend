8d150368037d72b084a4d9e9a7c386ccdfcd4b30
yasser <166193597+YasserDERDOUM@users.noreply.github.com>
2025-12-28 11:53:18 +0100

    Fix import statements and clean up ProductDetail

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import { API, useCart } from '../App';
@@ -306,5 +306,6 @@ const ProductDetail = () => {
     </div>
   );
 };
+
 
 export default ProductDetail;
